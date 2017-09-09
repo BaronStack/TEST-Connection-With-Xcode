3fd1973866cd5a257d35a0f02df6655735da2948
hosts <BaronStack@users.noreply.github.com>
2017-09-09 13:36:48 +0800

    First Commit

---
+++ thoughtWorks-homeWork/thoughtWorks-homeWork/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  thoughtWorks-homeWork
+//
+//  Created by MyFavour on 2017/9/9.
+//  Copyright © 2017年 MyFavour. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
